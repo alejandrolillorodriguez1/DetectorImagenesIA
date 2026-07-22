ca0d9e9acc9cfb89e350394d39f27cb888197a0d
alejandrolillorodriguez1 <alejandrolillorod.alr@gmail.com>
2026-07-22 12:47:33 +0200

    Preparar dataset y separar en test val y train

---
+++ training/src/dataset.py
@@ -0,0 +1,22 @@
+import torch
+import torchvision.datasets as datasets
+from torchvision import transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]) 
+])
+
+total_data = datasets.ImageFolder(root='C:/Users/aleja/Datasets/CIFAKE/train', transform=transform)
+total_train = len(total_data)
+train = int(total_train * 0.8)
+val = total_train - train
+
+generator = torch.Generator().manual_seed(42)
+
+train_dataset, val_dataset = torch.utils.data.random_split(total_data, [train, val], generator=generator)
+
+test_dataset = datasets.ImageFolder(root='C:/Users/aleja/Datasets/CIFAKE/test', transform=transform)
+
+
+
